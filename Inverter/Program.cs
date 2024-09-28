34ff6c667da55d912b4e7c3d4ecf2b7583ef87bd
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2024-09-28 19:07:50 -0300

    feat: project

---
+++ Inverter/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Inverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a string: ");
+                string input = Console.ReadLine() ?? "";
+                string invertedInput = StringReverse(input);
+                Console.WriteLine($"Reversed input: {invertedInput}");
+
+                Console.Write("\nTry again? (y/n) ");
+                string tryAgain = Console.ReadLine() ?? "";
+
+                if (tryAgain.ToLower() != "y")
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+
+        public static string StringReverse(string input)
+        {
+            char[] chars = input.ToCharArray();
+            string output = "";
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                output += chars[i];
+            }
+            return output;
+        }
+    }
+}
